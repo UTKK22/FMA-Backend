e9a6889486d2c12b8d4ddc8e2a54e500b4f6573b
Utkarsh Sharma <utkarshsharma@Utkarshs-MacBook-Air.local>
2024-12-03 19:08:33 +0530

    fifth commit

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cors({
 }));
 // app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); 
 
 const User = require('./models/User.js');
 const JWT_SECRET = "verifytoken";
